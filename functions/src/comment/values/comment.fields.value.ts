7108ca66b89ad88ca30d382f53828e2f20892b44
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-13 17:46:45 +0900

    feat : CommentFields 생성

---
+++ functions/src/comment/values/comment.fields.value.ts
@@ -0,0 +1,17 @@
+export const CommentFields = {
+    content: 'content',
+    uid: 'uid',
+    date: 'date',
+    likes: 'likes',
+    dislikes: 'dislikes',
+    replyTarget: 'replyTarget',
+};
+
+// export class CommentFields{
+//     static content = 'uid';
+//     static uid = 'uid';
+//     static date = 'date';
+//     static likes = 'likes';
+//     static dislikes = 'dislikes';
+//     static replyTarget = 'replyTarget';
+// }
