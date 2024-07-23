e9bd6fb6dddf20c463a4d2c5248f418ad63b8d72
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-23 15:18:35 +0900

    feat : getReviewDocument , getThumbnailImagePath 추가

--- functions/src/site_review/site_review.references.ts
+++ functions/src/site_review/site_review.references.ts
@@ -1,4 +1,4 @@
-import { CollectionReference } from "firebase-admin/firestore";
+import { CollectionReference, DocumentReference } from "firebase-admin/firestore";
 import * as firebaseAdmin from 'firebase-admin';
 
 export class SiteReviewReferences{
@@ -8,5 +8,13 @@ export class SiteReviewReferences{
 
     static getReviewImagePath(noticeId : string , docId : string) : string{
         return `site_review/${noticeId}/${docId}/`;
+    }
+
+    static getReviewDocument(noticeId : string , reviewId : string) : DocumentReference{
+      return firebaseAdmin.firestore().collection('site_review').doc(noticeId).collection('review').doc(reviewId);
+    }
+
+    static getThumbnailImagePath(noticeId : string , reviewId : string , thumbnailImageName : string) : string{
+      return `${SiteReviewReferences.getReviewImagePath(noticeId , reviewId)}/${thumbnailImageName}`
     }
 }
