da7fef3cdefa5df6d916ff99fbaf5dcec8976de6
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-07-27 20:47:58 +0900

    add : notice_dto.fields

---
+++ functions/src/notice/value/notice_dto.fields.ts
@@ -0,0 +1,11 @@
+export const NoticeDtoFields = {
+    noticeId : "noticeId",
+    views : "views",
+    likes : "likes",
+    like : "like",
+    scraps : "scraps",
+    houseName : "houseName",
+    info : "info",
+    applicationReceptionStartDate : "applicationReceptionStartDate", /// 청약 접수 시작일
+    recruitmentPublicAnnouncementDate : "recruitmentPublicAnnouncementDate", /// 모집 공고일
+};
