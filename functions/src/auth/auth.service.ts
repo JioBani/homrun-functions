27f717ea2119548d3240318eef6677b4924f8e44
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-08 23:00:54 +0900

    feat ; signUp() 추가, signIn() 변경

--- functions/src/auth/auth.service.ts
+++ functions/src/auth/auth.service.ts
@@ -2,11 +2,12 @@ import * as dotenv from 'dotenv';
 import * as firebaseAdmin from 'firebase-admin';
 import axios, { AxiosResponse } from 'axios';
 import { SocialProvider } from '../enum/social-provider.enum';
-import { UserDto } from '../model/user.dto';
 import {KakaoUserResponse} from '../model/kakao/kakao-user-resonse'
 import { NaverProfileResponse } from '../model/naver/naver-profile-response';
 import { BadRequestError, UnauthorizedError, ForbiddenError, NotFoundError, InternalServerError } from '../error/http.error';
 import { UserService } from '../user/user.service';
+import { Gender } from '../enum/gender.enum';
+import { Timestamp } from 'firebase-admin/firestore';
 
 //TODO error 코드
 
@@ -23,34 +24,25 @@ export class AuthService {
     //TODO 소셜 프로바이더 회원가입이 맞게 적용
     async signIn(accessToken : string, provider : SocialProvider) : Promise<string | undefined>{
 
-        var uid : string;
+        //#1. 토큰 검증 및 uid생성
+        var uid = await this.#verifyTokenAndCreateUid(accessToken , provider);
 
-        if(provider === SocialProvider.KAKAO){
-            var KakaoUserResponse : KakaoUserResponse = await this.getKakaoUserInfo(accessToken);
-            uid = `kakao:${KakaoUserResponse.id}`;
+        //#2. 유저가 있는지 확인
+        if(!await this.userService.isUserExist(uid)){
+          throw UnauthorizedError.UserNotFoundError();
         }
-        else if(provider === SocialProvider.NAVER){
-            var naverProfileResponse : NaverProfileResponse = await this.getNaverUserInfo(accessToken);
-            uid =  `naver:${naverProfileResponse.response.id}`;
-        }
-        else{
-            throw new BadRequestError({message : 'Incorrect social provider'})
-        }
-
-        //#. 유저 체크 및 없다면 회원가입
-        var userDto : UserDto = await this.userService.ensureUser(uid , provider);
   
-        //#2. 커스텀 토큰 생성
+        //#3. 커스텀 토큰 생성
         let token;
         try{   
-            token = await firebaseAdmin.auth().createCustomToken(userDto.uid);
+            token = await firebaseAdmin.auth().createCustomToken(uid);
             console.log(`[AuthService.signIn()] : ${token}`);
         }catch(error){
             console.log(`[AuthService.signIn()] 커스텀 토큰 제작중 오류 발생 : ${error}`);
             throw new InternalServerError({message : 'An unexpected error occurred during create custom token.'});
         }   
 
-        console.log(`[AuthService.signIn()] 로그인 완료 : ${userDto.socialProvider} , ${userDto.uid} , ${userDto.displayName} , ${token}}`)
+        console.log(`[AuthService.signIn()] 로그인 완료 : ${uid}}`)
         return token;
     }
 
@@ -140,5 +132,58 @@ export class AuthService {
               throw new Error('An unexpected error occurred.');
             }
           }
+    }
+
+    async signUp(data : {
+      accessToken : string, 
+      socialProvider : SocialProvider,
+      displayName : string,
+      gender : Gender,
+      birth : Timestamp
+    }) : Promise<string | undefined>{
+
+      //#1. 토큰 검증 및 uid 만들기
+      const uid = await this.#verifyTokenAndCreateUid(data.accessToken , data.socialProvider);
+
+      //#2. user 만들기
+      await this.userService.createUser({
+        uid : uid,
+        socialProvider : data.socialProvider,
+        displayName : data.displayName,
+        gender : data.gender,
+        birth : data.birth
+      });
+
+      //#3. 커스텀 토큰 생성
+      let token;
+      try{   
+          token = await firebaseAdmin.auth().createCustomToken(uid);
+          console.log(`[AuthService.signIn()] : ${token}`);
+      }catch(error){
+          console.log(`[AuthService.signIn()] 커스텀 토큰 제작중 오류 발생 : ${error}`);
+          throw new InternalServerError({message : 'An unexpected error occurred during create custom token.'});
+      }   
+
+      console.log(`[AuthService.signIn()] 회원가입 완료 : ${uid}`)
+      return token;
+    }
+
+    //#. 각 소셜에 token을 검증하고 uid를 가져옴
+    async #verifyTokenAndCreateUid(accessToken : string, socialProvider : SocialProvider) : Promise<string>{
+      let uid : string;
+
+      if(socialProvider === SocialProvider.KAKAO){
+          let KakaoUserResponse : KakaoUserResponse = await this.getKakaoUserInfo(accessToken);
+          uid = `kakao:${KakaoUserResponse.id}`;
+      }
+      else if(socialProvider === SocialProvider.NAVER){
+          let naverProfileResponse : NaverProfileResponse = await this.getNaverUserInfo(accessToken);
+          uid =  `naver:${naverProfileResponse.response.id}`;
+      }
+      else{
+          throw new BadRequestError({message : 'Incorrect social provider'})
+      }
+
+      return uid;
     }
 }
