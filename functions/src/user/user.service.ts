15f80d54dbe5ca86d15399dbdfe8007e395a56d3
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-08-08 23:00:25 +0900

    feat : #createUid() 추가 , #createUserDocuemt() ,  createUser()변경

--- functions/src/user/user.service.ts
+++ functions/src/user/user.service.ts
@@ -3,20 +3,31 @@ import { SocialProvider } from "../enum/social-provider.enum";
 import { UserReferences } from "./user.references";
 import { UserDto } from "../model/user.dto";
 import { Gender } from "../enum/gender.enum";
-import { InternalServerError } from '../error/http.error';
+import { ConflictError, InternalServerError } from '../error/http.error';
 import { logger } from 'firebase-functions/v1';
+import { Timestamp } from 'firebase-admin/firestore';
 
 export class UserService{
   
     //#. 유저 있는지 확인
-    async isUidExist(uid: string): Promise<boolean> {
+    async isUserExist(uid: string) : Promise<boolean>{
+        //#. uid
         try{
             await firebaseAdmin.auth().getUser(uid);
-            return true;
         }catch(e){
             return false;
         }
-    }    
+
+        //#. user doc
+        const userDoc = await UserReferences.getUserDocument(uid).get();
+
+        if (!userDoc.exists) {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 
     //#. 유저 가져오기
     async getUser(uid: string): Promise<UserDto | undefined> {
@@ -44,8 +55,42 @@ export class UserService{
       }
     }
 
+    async createUser(data : {
+        uid : string, 
+        socialProvider : SocialProvider,
+        displayName : string,
+        gender : Gender,
+        birth : Timestamp
+      }) : Promise<UserDto>{
+
+        //#1. uid가 있는지 확인하고 만들기        
+        if(await this.isUserExist(data.uid)){
+            throw ConflictError.UserAlreadyExistsError();
+        }
+        
+        await this.#createUid(data.uid);        
+
+        //#2. user document 만들기        
+        try{
+            const userDto = await this.#createUserDocument({
+                uid : data.uid, 
+                socialProvider : data.socialProvider,
+                displayName : data.displayName,
+                gender : data.gender,
+                birth : Timestamp.now()
+            });
+
+            console.log(`[UserService.ensureUser()] ${userDto.socialProvider} 유저 회원가입 : ${userDto.uid} , ${userDto.displayName}`);
+            return userDto;
+        }catch(e){
+            logger.error(`[UserService.ensureUser()] 유저 정보 문서 생성중 오류 : ${e}`);
+            await firebaseAdmin.auth().deleteUser(data.uid);
+            throw new InternalServerError({message : 'Error setting user document.'});
+        }         
+    }
+
     //#. 유저 만들기
-    async #createUser(uid : string){
+    async #createUid(uid : string){
         try{
             await firebaseAdmin.auth().createUser({
                 uid : uid,
@@ -62,66 +107,24 @@ export class UserService{
     }
 
     //#. 유저 문서 만들기
-    async #createUserDocument(uid : string,  socialProvider : SocialProvider) : Promise<UserDto>{
+    async #createUserDocument(data : {
+        uid : string, 
+        socialProvider : SocialProvider,
+        displayName : string,
+        gender : Gender,
+        birth : Timestamp
+      }) : Promise<UserDto>{
         var userDto = new UserDto({
-            uid: uid,
-            socialProvider : socialProvider,
-            displayName: this.#createDisplayName(),
+            uid: data.uid,
+            socialProvider : data.socialProvider,
+            displayName: data.displayName,
             birth: "2000-01-01",
-            gender: Gender.MALE,
+            gender: data.gender,
         }); 
 
-        await  UserReferences.getUserDocument(uid).set(userDto.toPlainObject(), { merge: true });  
+        await  UserReferences.getUserDocument(data.uid).set(userDto.toPlainObject(), { merge: true });  
         
         return userDto;
     }
-
-    //#. 유저가 있는지 확인하고 없으면 회원가입
-    async ensureUser(uid : string,  socialProvider : SocialProvider) : Promise<UserDto>{
-        var isUidExist = await this.isUidExist(uid);        
-        var userDto : UserDto | undefined = await this.getUser(uid);
-        
-        if(!isUidExist){
-            await this.#createUser(uid);
-        }
-        
-        if(!userDto){
-            try{
-                userDto = await this.#createUserDocument(uid , socialProvider);
-                console.log(`[UserService.ensureUser()] ${userDto.socialProvider} 유저 회원가입 : ${userDto.uid} , ${userDto.displayName}`);
-                return userDto;
-            }catch(e){
-                logger.error(`[UserService.ensureUser()] 유저 정보 문서 생성중 오류 : ${e}`);
-                await firebaseAdmin.auth().deleteUser(uid);
-                throw new InternalServerError({message : 'Error setting user document.'});
-            }
-        }
-        else{
-            return userDto;
-        }           
-    }
-
-    //#. 유저 이름 만들기
-    #createDisplayName() : string {
-        const adjectives = [
-            "춤추는", "용감한", "조용한", "신비한", "반짝이는",
-            "사나운", "온화한", "빛나는", "속삭이는", "기쁜",
-            "환한", "그림자", "황금빛", "마법의", "방랑하는",
-            "활기찬", "얼어붙은", "장난기 많은", "평온한", "웅장한"
-        ];
-
-        const nouns = [
-            "딸기", "사자", "강", "유령", "은하",
-            "불사조", "나비", "산", "고래", "유니콘",
-            "용", "숲", "별", "신비", "오아시스",
-            "호랑이", "오로라", "스핑크스", "파도", "성"
-        ];
-
-        const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-        const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-
-        return `${randomAdjective} ${randomNoun}`;
-    }
-
 }
 
