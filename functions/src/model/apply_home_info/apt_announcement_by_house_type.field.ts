7b623923a944b953607bb873a626e195cd3f29d1
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-11 15:42:16 +0900

    add : apt_announcement_by_house_type.fieds.ts

---
+++ functions/src/model/apply_home_info/apt_announcement_by_house_type.field.ts
@@ -0,0 +1,90 @@
+/**
+ * APT 분양정보 주택형별 상세조회 필드 매핑
+ */
+export const AptAnnouncementByHouseTypeField = {
+    /**
+     * 주택 관리 번호 (주택관리번호).
+     */
+    houseManageNumber: "HOUSE_MANAGE_NO",
+  
+    /**
+     * 공고 번호 (공고번호).
+     */
+    publicNoticeNumber: "PBLANC_NO",
+  
+    /**
+     * 모델 번호 (모델번호).
+     */
+    modelNumber: "MODEL_NO",
+  
+    /**
+     * 주택 형 (주택형).
+     */
+    houseType: "HOUSE_TY",
+  
+    /**
+     * 공급 면적 (공급면적).
+     */
+    supplyArea: "SUPLY_AR",
+  
+    /**
+     * 일반 공급 세대 수 (일반공급세대수).
+     */
+    generalSupplyHouseholds: "SUPLY_HSHLDCO",
+  
+    /**
+     * 특별 공급 세대 수 (특별공급세대수).
+     */
+    specialSupplyHouseholds: "SPSPLY_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 다자녀 가구 세대 수 (특별공급-다자녀가구 세대수).
+     */
+    multiChildHouseholds: "MNYCH_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 신혼부부 세대 수 (특별공급-신혼부부 세대수).
+     */
+    newlywedHouseholds: "NWWDS_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 생애 최초 세대 수 (특별공급-생애최초 세대수).
+     */
+    firstTimeHouseholds: "LFE_FRST_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 노부모 부양 세대 수 (특별공급-노부모부양 세대수).
+     */
+    elderlyParentSupportHouseholds: "OLD_PARNTS_SUPORT_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 기관 추천 세대 수 (특별공급-기관추천 세대수).
+     */
+    institutionRecommendedHouseholds: "INSTT_RECOMEND_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 기타 세대 수 (특별공급-기타 세대수).
+     */
+    otherSpecialHouseholds: "ETC_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 이전 기관 세대 수 (특별공급-이전기관 세대수).
+     */
+    relocatedInstitutionHouseholds: "TRANSR_INSTT_ENFSN_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 청년 세대 수 (특별공급-청년 세대수).
+     */
+    youthHouseholds: "YGMN_HSHLDCO",
+  
+    /**
+     * 특별 공급 - 신생아 세대 수 (특별공급-신생아 세대수).
+     */
+    newbornHouseholds: "NWBB_HSHLDCO",
+  
+    /**
+     * 공급 금액 (분양최고금액) (단위: 만원).
+     */
+    highestSupplyPrice: "LTTOT_TOP_AMOUNT"
+};    
+  
