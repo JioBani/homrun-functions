bcbacdd7cb0a6c048b50487a275efa784e572ef0
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-11 15:43:16 +0900

    feat : isUpdated와 isByHouseTypeInfoSuccess 필드 추가

--- functions/src/feature/notice/model/notice_document_result.ts
+++ functions/src/feature/notice/model/notice_document_result.ts
@@ -2,12 +2,19 @@ export class NoticeDocumentResult{
     noticeId : string;
     isSuccess : boolean;
     isGenerated : boolean;
+    isUpdated : boolean;
+    isByHouseTypeInfoSuccess : boolean;
     error : any;
 
-    constructor({ noticeId, isSuccess,isGenerated,error } : { noticeId: string; isSuccess: boolean; isGenerated : boolean,error?: any }) {
+    constructor(
+        { noticeId, isSuccess,isGenerated,isUpdated,isByHouseTypeInfoSuccess,error } : 
+        { noticeId: string; isSuccess: boolean; isGenerated : boolean , isUpdated : boolean , isByHouseTypeInfoSuccess : boolean,error?: any }
+    ) {
         this.noticeId = noticeId;
         this.isSuccess = isSuccess;
         this.isGenerated = isGenerated;
+        this.isUpdated = isUpdated;
+        this.isByHouseTypeInfoSuccess = isByHouseTypeInfoSuccess;
         this.error = error;
     }
 }
