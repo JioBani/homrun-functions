79a0bc1d7a0ad0d4be3c6d6388ce28fff5c812ff
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2024-09-11 19:55:47 +0900

    feat : isProcessedAPTAnnouncementByHouseTypeSuccess, isProcessedAPTAnnouncementByHouseTypeHasCountError 필드 추가

--- functions/src/feature/notice/model/notice_document_result.ts
+++ functions/src/feature/notice/model/notice_document_result.ts
@@ -4,17 +4,39 @@ export class NoticeDocumentResult{
     isGenerated : boolean;
     isUpdated : boolean;
     isByHouseTypeInfoSuccess : boolean;
+    isProcessedAPTAnnouncementByHouseTypeSuccess : boolean;
+    isProcessedAPTAnnouncementByHouseTypeHasCountError : boolean;
     error : any;
 
     constructor(
-        { noticeId, isSuccess,isGenerated,isUpdated,isByHouseTypeInfoSuccess,error } : 
-        { noticeId: string; isSuccess: boolean; isGenerated : boolean , isUpdated : boolean , isByHouseTypeInfoSuccess : boolean,error?: any }
+        { 
+            noticeId, 
+            isSuccess,
+            isGenerated,
+            isUpdated,
+            isByHouseTypeInfoSuccess,
+            isProcessedAPTAnnouncementByHouseTypeSuccess,
+            isProcessedAPTAnnouncementByHouseTypeHasCountError,
+            error 
+        } : 
+        { 
+            noticeId: string, 
+            isSuccess: boolean,
+            isGenerated : boolean, 
+            isUpdated : boolean , 
+            isByHouseTypeInfoSuccess :boolean,
+            isProcessedAPTAnnouncementByHouseTypeSuccess : boolean,
+            isProcessedAPTAnnouncementByHouseTypeHasCountError : boolean
+            error?: any 
+        }
     ) {
         this.noticeId = noticeId;
         this.isSuccess = isSuccess;
         this.isGenerated = isGenerated;
         this.isUpdated = isUpdated;
         this.isByHouseTypeInfoSuccess = isByHouseTypeInfoSuccess;
+        this.isProcessedAPTAnnouncementByHouseTypeSuccess = isProcessedAPTAnnouncementByHouseTypeSuccess;
+        this.isProcessedAPTAnnouncementByHouseTypeHasCountError = isProcessedAPTAnnouncementByHouseTypeHasCountError;
         this.error = error;
     }
 }
